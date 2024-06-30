7b7f0cc36598053880ec3079dd7d26500d12e72e
Betül Bedel <bedel69@hotmail.com>
2024-06-30 19:08:20 +0300

    Abstractions

--- Blog.Service/Services/Abstractions/IArticleService.cs
+++ Blog.Service/Services/Abstractions/IArticleService.cs
@@ -10,6 +10,18 @@ namespace Blog.Service.Services.Abstractions
 {
     public interface IArticleService
     {
-        Task<List<ArticleDto>> GetAllArticleAsync();
+        Task<List<ArticleDto>> GetAllArticlesWithCategoryNonDeletedAsync();
+        Task<List<ArticleDto>> GetAllArticlesWithCategoryDeletedAsync();
+        Task<ArticleDto> GetArticlesWithCategoryNonDeletedAsync(Guid articleId);
+        Task CreateArticleAsync(ArticleAddDto articleAddDto);
+
+        Task<string> UpdateArticleAsync(ArticleUpdateDto articleUpdateDto);
+        Task<string> SafeDeleteArticleAsync(Guid articleId);
+        Task<string> UndoDeleteArticleAsync(Guid articleId);
+
+        Task<ArticleListDto> GetAllByPagingAsync(Guid? categoryId, int currentPage = 1, int pageSize = 3,
+            bool isAscending = false);
+        Task<ArticleListDto> SearchAsync(string keyword, int currentPage = 1, int pageSize = 3,
+            bool isAscending = false);
     }
 }
---
+++ Blog.Service/Services/Abstractions/IDashboradService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Service.Services.Abstractions
+{
+    public interface IDashboradService
+    {
+        Task<List<int>> GetYearlyCounts();
+        Task<int> GetTotalArticleCount();
+        Task<int> GetTotalCategoryCount();
+    }
+}
