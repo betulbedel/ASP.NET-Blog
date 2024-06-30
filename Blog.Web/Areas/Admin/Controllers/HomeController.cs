d500a92406bfb55c6716a0109f573bcc17619334
Betül Bedel <bedel69@hotmail.com>
2024-06-30 19:08:34 +0300

    Controllers

--- Blog.Web/Areas/Admin/Controllers/HomeController.cs
+++ Blog.Web/Areas/Admin/Controllers/HomeController.cs
@@ -1,21 +1,48 @@
-﻿using Blog.Service.Services.Abstractions;
+﻿using Blog.Entity.Entities;
+using Blog.Service.Services.Abstractions;
+using Blog.Service.Services.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Blog.Web.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly IArticleService articleService;
-        public HomeController(IArticleService articleService)
+        private readonly IDashboradService dashboradService;
+
+        public HomeController(IArticleService articleService, IDashboradService dashboradService)
         {
             this.articleService = articleService;
-
+            this.dashboradService = dashboradService;
         }
         public async Task<IActionResult> Index()
         {
-            var articles =  await articleService.GetAllArticleAsync();
+            var articles = await articleService.GetAllArticlesWithCategoryNonDeletedAsync();
+
             return View(articles);
+        }
+        [HttpGet]
+        public async Task<IActionResult> YearlyArticleCounts()
+        {
+            var count = await dashboradService.GetYearlyCounts();
+            return Json(JsonConvert.SerializeObject(count));
+        }
+        [HttpGet]
+        public async Task<IActionResult> TotalArticleCount()
+        {
+            var count = await dashboradService.GetTotalArticleCount();
+            return Json(count);
+        }
+        [HttpGet]
+        public async Task<IActionResult> TotalCategoryCount()
+        {
+            var count = await dashboradService.GetTotalCategoryCount();
+            return Json(count);
         }
     }
 }
